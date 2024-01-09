4c6ef679597037606ee31c0066729a1ef970972d
adyfp24 <adyfp24@gmail.com>
2024-01-09 12:49:34 +0700

    feat(login): add login method

--- app/Http/Controllers/api/auth/LoginController.php
+++ app/Http/Controllers/api/auth/LoginController.php
@@ -4,10 +4,29 @@ namespace App\Http\Controllers\api\auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash; // Tambahkan use statement untuk Hash
 
 class LoginController extends Controller
 {
-    public function login(){
-        
+    public function login(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('username', $request->username)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'username' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $api_token = $user->createToken('api_token')->plainTextToken;
+
+        return response()->json(['message' => 'login sukses', 'user' => $user, 'token' => $api_token], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\auth\LoginController;
 use App\Http\Controllers\api\auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('register', [RegisterController::class, 'register']);
-Route::post('register/remaja', [RegisterController::class, 'registRemaja']);
+Route::post('login', [LoginController::class, 'login']);
+
