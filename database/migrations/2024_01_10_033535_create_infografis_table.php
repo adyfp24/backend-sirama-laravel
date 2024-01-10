718e497cac8df373317fe02206e194403a747256
NeoBitose <ramadhanalif430@gmail.com>
2024-01-10 12:37:36 +0700

    feat(migration): add migration infografis & fav_infografis

---
+++ database/migrations/2024_01_10_033535_create_infografis_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInfografisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('infografis', function (Blueprint $table) {
+            $table->id('id_infografis');
+            $table->string('judul_infografis');
+            $table->text('deskripsi_infografis');
+            $table->date('tgl_upload');
+            $table->text('gambar_infografis');
+            $table->bigInteger('upload_user_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('upload_user_id')->references('id_user')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('infografis');
+    }
+}
