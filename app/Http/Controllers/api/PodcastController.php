a8003a767b251cb182ab6c4de6a267f252f34453
adyfp24 <adyfp24@gmail.com>
2024-01-11 00:01:21 +0700

    fix(podcast): fixing try catch and eror handling api

--- app/Http/Controllers/api/PodcastController.php
+++ app/Http/Controllers/api/PodcastController.php
@@ -13,87 +13,269 @@ class PodcastController extends Controller
 {
     public function getAllPodcast()
     {
-        $allPodcast = Podcast::all();
-        if ($allPodcast) {
-            return response()->json(['message' => 'data podcast tersedia', 'podcast' => $allPodcast], 200);
-        } else {
-            return response()->json(['message' => 'data podcast tidak tersedia', 'podcast' => $allPodcast], 404);
+        $status = '';
+        $message = '';
+        $data = '';
+        $status_code = 200;
+        try {
+            $allPodcast = Podcast::all();
+            if ($allPodcast) {
+                $message = 'data podcast tersedia';
+            } else {
+                $message = 'data podcast tidak tersedia';
+            }
+            $status = 'success';
+            $data = $allPodcast;
+        } catch (\Exception $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } catch (\Illuminate\Database\QueryException $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } finally {
+            return response()->json([
+                'status' => $status,
+                'message' => $message,
+                'data' => $data
+            ], $status_code);
         }
     }
     public function getPodcastById($id)
     {
-        $podcast = Podcast::where('id_podcast', $id)->first();
-        if ($podcast) {
-            return response()->json(['message' => 'data podcast tersedia', 'podcast' => $podcast], 200);
-        } else {
-            return response()->json(['message' => 'data podcast tidak tersedia'], 404);
+        $status = '';
+        $message = '';
+        $data = '';
+        $status_code = 200;
+        try {
+            $podcast = Podcast::where('id_podcast', $id)->first();
+            if ($podcast) {
+                $message = 'data podcast tersedia';
+            } else {
+                $message = 'data podcast tidak tersedia';
+            }
+            $status = 'success';
+            $data = $podcast;
+        } catch (\Exception $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } catch (\Illuminate\Database\QueryException $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } finally {
+            return response()->json([
+                'status' => $status,
+                'message' => $message,
+                'data' => $data
+            ], $status_code);
         }
     }
     public function addPodcast(Request $request)
     {
-        $user = auth()->user();
-        $newPodcast = Podcast::create([
-            'judul_podcast' => $request->judul_podcast,
-            'link_podcast' => $request->link_podcast,
-            'tgl_upload' => $request->tgl_upload,
-            'upload_user_id' => $user->id_user
-        ]);
-        return response()->json(['message' => 'podcast berhasil dibuat', 'podcast' => $newPodcast], 200);
+        $status = '';
+        $message = '';
+        $data = '';
+        $status_code = 200;
+        try {
+            $user = auth()->user();
+            $newPodcast = Podcast::create([
+                'judul_podcast' => $request->judul_podcast,
+                'link_podcast' => $request->link_podcast,
+                'tgl_upload' => $request->tgl_upload,
+                'upload_user_id' => $user->id_user
+            ]);
+            if ($newPodcast) {
+                $message = 'podcast berhasil ditambah';
+            }else{
+                $message = 'podcast agal ditambah';
+            }
+            $status = 'success';
+            $data = $newPodcast;
+        } catch (\Exception $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } catch (\Illuminate\Database\QueryException $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } finally {
+            return response()->json([
+                'status' => $status,
+                'message' => $message,
+                'data' => $data
+            ], $status_code);
+        }
     }
     public function updatePodcast(Request $request, $id)
     {
-        $user = auth()->user();
-        $podcast = Podcast::find($id);
-        $updatedPodcast = $podcast->update([
-            'judul_podcast' => $request->judul_podcast,
-            'link_podcast' => $request->link_podcast,
-            'tgl_upload' => $request->tgl_upload,
-            'upload_user_id' =>$user->id_user
-        ]);
-        if ($updatedPodcast) {
-            return response()->json(['message' => 'podcast berhasil di update', 'updated podcast' => $podcast], 200);
+        $status = '';
+        $message = '';
+        $data = '';
+        $status_code = 200;
+        try{
+            $user = auth()->user();
+            $podcast = Podcast::find($id);
+            $updatedPodcast = $podcast->update([
+                'judul_podcast' => $request->judul_podcast,
+                'link_podcast' => $request->link_podcast,
+                'tgl_upload' => $request->tgl_upload,
+                'upload_user_id' => $user->id_user
+            ]);
+            if ($updatedPodcast) {
+                $message = 'podcast berhasil di update';
+            }else{
+                $message = 'podcast gagal diupdate';
+            }
+            $status = 'success';
+            $data = $updatedPodcast;
+        }catch (\Exception $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } catch (\Illuminate\Database\QueryException $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } finally {
+            return response()->json([
+                'status' => $status,
+                'message' => $message,
+                'data' => $data
+            ], $status_code);
         }
     }
     public function deletePodcast($id)
-    {
-        $podcast = Podcast::find($id);
-        $deletedPodcast = $podcast->delete();
-        if ($deletedPodcast) {
-            return response()->json(['message' => 'podcast berhasil dihapus'], 200);
-        } else {
-            return response()->json(['message' => 'podcast gagal dihapus'], 401);
+    {   
+        $status = '';
+        $message = '';
+        $data = '';
+        $status_code = 200;
+        try{
+            $podcast = Podcast::find($id);
+            $deletedPodcast = $podcast->delete();
+            if ($deletedPodcast) {
+                $message = 'podcast berhasil dihapus';
+            } else {
+                $message = 'podcast gagal dihapus';
+            }
+            $status = 'succes';
+            $data = $deletedPodcast;
+        }catch (\Exception $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } catch (\Illuminate\Database\QueryException $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } finally {
+            return response()->json([
+                'status' => $status,
+                'message' => $message,
+                'data' => $data
+            ], $status_code);
         }
     }
-    public function getAllFavPodcast(){
-        $allFavPodcast = FavPodcast::all();
-        if ($allFavPodcast) {
-            return response()->json(['message' => 'data podcast favorit tersedia', 'podcast' => $allFavPodcast], 200);
-        } else {
-            return response()->json(['message' => 'data podcast favorit tidak tersedia', 'podcast' => $allFavPodcast], 404);
+    public function getAllFavPodcast()
+    {
+        $status = '';
+        $message = '';
+        $data = '';
+        $status_code = 200;
+        try{
+            $allFavPodcast = FavPodcast::all();
+            if ($allFavPodcast) {
+                $message = 'data podcast favorit tersedia';
+            } else {
+                $message = 'data podcast favorit tidak tersedia';
+            }
+            $status = 'success';
+            $data = $allFavPodcast;
+        }catch (\Exception $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } catch (\Illuminate\Database\QueryException $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } finally {
+            return response()->json([
+                'status' => $status,
+                'message' => $message,
+                'data' => $data
+            ], $status_code);
         }
     }
     public function addFavPodcast($id)
     {
-        $user = auth()->user();
-        $favPodcast = FavPodcast::create([
-            'podcast_id' => $id,
-            'user_id' => $user->id_user
-        ]);
-        if ($favPodcast) {
-            return response()->json(['message' => 'podcast berhasil ditambah ke favorit', 'fav podcast' => $favPodcast], 200);
-        }else{
-            return response()->json(['message' => 'podcast gagal ditambah ke favorit'], 401);
+        $status = '';
+        $message = '';
+        $data = '';
+        $status_code = 200;
+        try{
+            $user = auth()->user();
+            $favPodcast = FavPodcast::create([
+                'podcast_id' => $id,
+                'user_id' => $user->id_user
+            ]);
+            if ($favPodcast) {
+                $message = 'podcast berhasil ditambah ke favorit';
+            } else {
+                $message = 'podcast gagal ditambah ke favorit';
+            }
+            $status = 'success';
+            $data = $favPodcast;
+        }catch (\Exception $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } catch (\Illuminate\Database\QueryException $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } finally {
+            return response()->json([
+                'status' => $status,
+                'message' => $message,
+                'data' => $data
+            ], $status_code);
         }
     }
     public function removeFavPodcast($id)
     {
-        $user = auth()->user();
-        $podcast = FavPodcast::find($id);
-        $removedPodcast = $podcast->delete();
-        if($removedPodcast){
-            return response()->json(['message' => 'podcast berhasil dihapus dari favorit'], 200);
-        }else{
-            return response()->json(['message' => 'podcast gagal dihapus dari favorit'], 401);
+        $status = '';
+        $message = '';
+        $data = '';
+        $status_code = 200;
+        try{
+            $podcast = FavPodcast::find($id);
+            $removedPodcast = $podcast->delete();
+            if ($removedPodcast) {
+                $message = 'podcast berhasil dihapus dari favorit';
+            } else {
+                $message = 'podcast gagal dihapus dari favorit';
+            }
+            $status = 'success';
+            $data = $removedPodcast;
+        }catch (\Exception $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } catch (\Illuminate\Database\QueryException $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } finally {
+            return response()->json([
+                'status' => $status,
+                'message' => $message,
+                'data' => $data
+            ], $status_code);
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -23,13 +23,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-#authentication routes
-
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [LoginController::class, 'login'])->name('login');
 Route::post('logout', [LogoutController::class, 'logout'])->middleware('auth:sanctum');
 
-#crud podcast routes
 
 Route::middleware(['auth:sanctum'])->group(function(){
     Route::post('podcast', [PodcastController::class, 'addPodcast']);
