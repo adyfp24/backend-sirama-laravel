a2897c4f1515717a2fdc3286b416c1564fdfa339
NeoBitose <ramadhanalif430@gmail.com>
2024-01-12 13:55:04 +0700

    feat(film, fav_film): add model

--- app/Models/FavFilm.php
+++ app/Models/FavFilm.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class FavFilm extends Model
 {
     use HasFactory;
+    protected $fillable = ['user_id', 'film_id'];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function film(){
+        return $this->belongsTo(Film::class);
+    }
 }
