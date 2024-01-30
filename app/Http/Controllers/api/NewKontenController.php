28812b761cd01a4e721ea0bd971b6a8836c7b868
adyfp24 <adyfp24@gmail.com>
2024-01-30 10:45:35 +0700

    feat (konten terbaru): adding endpoint for get all new konten

--- app/Http/Controllers/api/NewKontenController.php
+++ app/Http/Controllers/api/NewKontenController.php
@@ -3,11 +3,43 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Film;
+use App\Models\Podcast;
+use App\Models\VideoEdukasi;
 use Illuminate\Http\Request;
 
 class NewKontenController extends Controller
 {
-    public function getAllNewKonten(){
-        // join video, film, quote, podcast terbaru (descending) 
+    public function getAllNewKonten()
+    {
+        $status = '';
+        $message = '';
+        $data = '';
+        $status_code = 200;
+        try {
+            $films = Film::orderByDesc('tgl_upload')->limit(2)->get();
+            $podcasts = Podcast::orderByDesc('tgl_upload')->limit(2)->get();
+            $videos = VideoEdukasi::orderByDesc('tgl_upload')->limit(2)->get();
+            $result = $films->merge($podcasts)->merge($videos);
+            $message = 'data konten terbaru berhasil didapat';
+            $status = 'success';
+            $data = $result;
+        } catch (\Exception $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } catch (\Illuminate\Database\QueryException $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } finally {
+            return response()->json([
+                'status' => $status,
+                'message' => $message,
+                'data' => $data
+            ], $status_code);
+        }
+        // Mendapatkan 5 konten terbaru dari tabel Fil
+
     }
 }
