b0f76743fd4e49b0cb7e6994c27a2ede97ae4835
NeoBitose <ramadhanalif430@gmail.com>
2024-02-15 15:09:18 +0700

    feat(skrinning): add function riwayat

--- app/Http/Controllers/SkrinningController.php
+++ app/Http/Controllers/SkrinningController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\BagianSkrinning;
+use App\Models\BagianSkrinningUser;
+use App\Models\HasilSkrinning;
 use App\Models\JawabanSkrinning;
+use App\Models\RiwayatHasilSkrinning;
+use App\Models\RiwayatSkrinning;
 use App\Models\Skrinning;
 use App\Models\SkrinningUser;
 use App\Models\SoalSkrinning;
@@ -199,24 +203,121 @@ class SkrinningController extends Controller
         }
     }
 
-    public function addRiwayatSkrinning(Request $request)
+    // public function getHasilSkrinning($id)
+    // {
+    //     $status = '';
+    //     $message = '';
+    //     $data = '';
+    //     $status_code = 200;
+    //     try {
+    //         $hasilSkrinning = HasilSkrinning::all();
+    //         if (count($hasilSkrinning) > 0) {
+    //             $message = 'data jenis skrinning tersedia';
+    //         } else {
+    //             $message = 'data jenis skrinning tidak tersedia';
+    //         }
+    //         $status = 'success';
+    //         $data = $hasilSkrinning; 
+    //     } catch (\Exception $e) {
+    //         $status = 'failed';
+    //         $message = 'Gagal menjalankan request. ' . $e->getMessage();
+    //         $status_code = $e->getCode();
+    //     } catch (\Illuminate\Database\QueryException $e) {
+    //         $status = 'failed';
+    //         $message = 'Gagal menjalankan request. ' . $e->getMessage();
+    //         $status_code = $e->getCode();
+    //     } finally {
+    //         return response()->json([
+    //             'status' => $status,
+    //             'message' => $message,
+    //             'data' => $data
+    //         ], $status_code);
+    //     }
+    // }
+
+    public function getRiwayatSkrinning(Request $request)
     {
-
-    }
-
-    public function addRiwayatHasilSkrinning(Request $request)
-    {
-
-    }
-
-    public function getRiwayatSkrinningUser()
-    {
-
+        $status = '';
+        $message = '';
+        $data = '';
+        $status_code = 200;
+        try {
+            $user = auth()->user();
+            $allSkrinning = DB::table('skrinning_users')
+                                ->where('user_id', $user->id_user)
+                                ->join('users', 'skrinning_users.user_id', '=', 'users.id_user')
+                                ->join('skrinnings', 'skrinning_users.skrinning_id', '=', 'skrinnings.id_skrinning')
+                                ->orderBy('tgl_pengisian', 'ASC')
+                                ->get();
+            if (count($allSkrinning) > 0) {
+                $message = 'data jenis skrinning tersedia';
+            } else {
+                $message = 'data jenis skrinning tidak tersedia';
+            }
+            $status = 'success';
+            $data = $allSkrinning;
+        } catch (\Exception $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } catch (\Illuminate\Database\QueryException $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request. ' . $e->getMessage();
+            $status_code = $e->getCode();
+        } finally {
+            return response()->json([
+                'status' => $status,
+                'message' => $message,
+                'data' => $data
+            ], $status_code);
+        }
     }
 
     public function getDetailRiwayatSkrinning($id)
     {
+        {
+            $status = '';
+            $message = '';
+            $data = '';
+            $status_code = 200;
+            try {
+                $detail = [];
+                $bagian = BagianSkrinningUser::join('skrinning_users', 'bagian_skrinning_users.skrin_user_id', '=', 'skrinning_users.id_skrin_user')
+                                                ->join('bagian_skrinnings', 'bagian_skrinning_users.bagian_skrinning_id', '=', 'bagian_skrinnings.id_bagian_skrinning')
+                                                ->where('skrin_user_id', $id)
+                                                ->get();
+                for ($a=0; $a < $bagian; $a++) { 
+                    $detail[$a][0] = [$bagian[$a]->nama_bagian, $bagian[$a]->tgl_pengisian] ;
+                    $riwayat = RiwayatSkrinning::where('bag_skrin_user_id', $bagian[$a]->id_bag_skrin_user)->get();
+                    for ($b=0; $b < $riwayat; $b++) { 
+                        $detail[$a][1][$b] = [$riwayat[$b]->soal, $riwayat[$b]->jawaban];
+                    }
+                    $riwayathasil = RiwayatHasilSkrinning::where('bag_skrin_user_id', $bagian[$a]->id_bag_skrin_user)->first();
+                    $detail[$a][2] = $riwayathasil->hasil;
+                }
 
+                if (count($detail) > 0) {
+                    $message = 'data terkait skrining tersedia';
+                } else {
+                    $message = 'data terkait skrining tidak tersedia';
+                }
+                $status = 'success';
+                $data = $detail;
+            } catch (\Exception $e) {
+                $status = 'failed';
+                $message = 'Gagal menjalankan request. ' . $e->getMessage();
+                $status_code = $e->getCode();
+            } catch (\Illuminate\Database\QueryException $e) {
+                $status = 'failed';
+                $message = 'Gagal menjalankan request. ' . $e->getMessage();
+                $status_code = $e->getCode();
+            } finally {
+                return response()->json([
+                    'status' => $status,
+                    'message' => $message,
+                    'data' => $data, 
+                ], $status_code);
+            }
+        }
     }
-    
 }
