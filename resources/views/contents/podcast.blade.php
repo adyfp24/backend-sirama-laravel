423e70d8fb0ccd5fbfe3934a1f1340494149e8ac
adyfp24 <adyfp24@gmail.com>
2024-02-16 23:14:48 +0700

    feat (dashboard admin): adding ajax for read podcast

--- resources/views/contents/podcast.blade.php
+++ resources/views/contents/podcast.blade.php
@@ -274,76 +274,15 @@
                     <table class="w-full border-collapse bg-white text-left text-sm text-gray-500">
                         <thead class="bg-gray-50">
                             <tr>
-                                <th scope="col" class="px-6 py-4 font-medium text-gray-900">Nama</th>
-                                <th scope="col" class="px-6 py-4 font-medium text-gray-900">Sertifikasi</th>
-                                <th scope="col" class="px-6 py-4 font-medium text-gray-900">Deskripsi</th>
-                                <th scope="col" class="px-6 py-4 font-medium text-gray-900">Minat Penelitian</th>
-                                <th scope="col" class="px-6 py-4 font-medium text-gray-900">Hasil Penelitian</th>
+                                <th scope="col" class="px-6 py-4 font-medium text-gray-900">No</th>
+                                <th scope="col" class="px-6 py-4 font-medium text-gray-900">Judul Podcast</th>
+                                <th scope="col" class="px-6 py-4 font-medium text-gray-900">Link Podcast</th>
+                                <th scope="col" class="px-6 py-4 font-medium text-gray-900">Jumlah Likes</th>
+                                <th scope="col" class="px-6 py-4 font-medium text-gray-900">Tanggal Upload</th>
                                 <th scope="col" class="px-6 py-4 font-medium text-gray-900">Aksi</th>
                             </tr>
                         </thead>
-                        <tbody class="divide-y divide-gray-100 border-t border-gray-100">
-
-                            <tr key={id} class="hover:bg-gray-50">
-                                <th class="px-6 py-4">
-                                    <div class="text-sm">
-                                        <div class="font-medium text-gray-700">{data.namamentor}</div>
-                                        <div class="text-gray-400">{data.spesialisasi}</div>
-                                        <div class="text-gray-400">{data.pendidikan}</div>
-                                        <div class="text-gray-400">Rp {data.fee}</div>
-                                    </div>
-                                </th>
-                                <td class="px-6 py-4">
-                                    {data.sertifikasi}
-                                </td>
-                                <td class="px-6 py-4">
-                                    <div class="text-sm">
-                                        <div class="font-medium text-gray-700">{data.deskripsimentor}</div>
-                                    </div>
-                                </td>
-                                <td class="px-6 py-4">
-                                    <div class="flex gap-2">
-                                        <span
-                                            class="inline-flex items-center gap-1 rounded-full bg-violet-50 px-2 py-1 text-xs font-semibold text-violet-600">
-                                            {data.minatpenelitian}
-                                        </span>
-                                    </div>
-                                </td>
-                                <td class="px-6 py-4">
-                                    <div class="flex gap-2">
-                                        <span
-                                            class="inline-flex items-center gap-1 rounded-full bg-violet-50 px-2 py-1 text-xs font-semibold text-violet-600">
-                                            {data.hasilpenelitian}
-                                        </span>
-                                    </div>
-                                </td>
-                                <td class="px-6 py-4">
-                                    <div class="flex gap-4">
-                                        <button onClick="">
-                                            <a x-data="{ tooltip: 'Delete' }" href="#">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                    class="h-6 w-6" x-tooltip="tooltip">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                                                </svg>
-                                            </a>
-                                        </button>
-
-                                        <button>
-                                            <a x-data="{ tooltip: 'Edit' }" href="#">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                    class="h-6 w-6" x-tooltip="tooltip">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-                                                </svg>
-                                            </a>
-                                        </button>
-                                        </Link>
-                                    </div>
-                                </td>
-                            </tr>
+                        <tbody id="podcast-list" class="divide-y divide-gray-100 border-t border-gray-100">
 
                         </tbody>
                     </table>
@@ -356,28 +295,39 @@
     </ul>
     <script>
         // Menggunakan jQuery AJAX untuk mengambil data dari API
-        // $.ajax({
-        //     url: 'http://127.0.0.1:8000/api/podcast',
-        //     method: 'GET',
-        //     dataType: 'json',
-        //     success: function(data) {
-        //         // Memasukkan data ke dalam templat Blade menggunakan jQuery
-        //         const podcastList = $('#podcastList');
-        //         $.each(data.data, function(index, podcast) {
-        //             const listItem = $('<li><br>');
-        //             listItem.append('<strong>ID:</strong> ' + podcast.id_podcast + '<br>');
-        //             listItem.append('<strong>Judul:</strong> ' + podcast.judul_podcast + '<br>');
-        //             listItem.append('<strong>Link:</strong> <a href="' + podcast.link_podcast +
-        //             '" target="_blank">' + podcast.link_podcast + '</a><br>');
-        //             listItem.append('<strong>Jumlah like:</strong> ' + podcast.total_likes + '<br>');
-        //             listItem.append('<strong>Tanggal Upload:</strong> ' + podcast.tgl_upload + '<br>');
-        //             podcastList.append(listItem);
-        //         });
-        //     },
-        //     error: function(xhr, status, error) {
-        //         console.error('Ada kesalahan:', error);
-        //     }
-        // });
+        $.ajax({
+            url: 'http://127.0.0.1:8000/api/podcast',
+            method: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                const podcastList = $('#podcast-list');
+                $.each(data.data, function(index, podcast) {
+                    const row = $('<tr>').addClass('hover:bg-gray-50');
+                    row.append('<td class="px-2 py-4">' + (index + 1) + '</td>');
+                    row.append('<td class="px-6 py-4">' + podcast.judul_podcast + '</td>');
+                    row.append('<td class="px-6 py-4"><div class="text-sm"><div class="font-medium text-gray-700">' + podcast.link_podcast + '</div></div></td>');
+                    row.append('<td class="px-6 py-4"><div class="flex gap-2"><span class="inline-flex items-center gap-1 rounded-full bg-violet-50 px-2 py-1 text-xs font-semibold text-violet-600">' + podcast.total_likes + '</span></div></td>');
+                    row.append('<td class="px-6 py-4"><div class="flex gap-2"><span class="inline-flex items-center gap-1 rounded-full bg-violet-50 px-2 py-1 text-xs font-semibold text-violet-600">' + podcast.tgl_upload + '</span></div></td>');
+                    row.append('<td class="px-6 py-4"><div class="flex gap-4"><button id="deleteButton" data-id="' + podcast.id_podcast + '">Delete</button><button id="editButton" data-id="' + podcast.id_podcast + '">Edit</button></div></td>');
+                    podcastList.append(row);
+                });
+    
+                // Event handling untuk tombol Delete
+                $('#deleteButton').on('click', function() {
+                    const podcastId = $(this).data('id');
+                    // Tambahkan logika untuk menghandle penghapusan podcast dengan ID tertentu
+                });
+    
+                // Event handling untuk tombol Edit
+                $('#editButton').on('click', function() {
+                    const podcastId = $(this).data('id');
+                    // Tambahkan logika untuk menghandle pengeditan podcast dengan ID tertentu
+                });
+            },
+            error: function(xhr, status, error) {
+                console.error('Ada kesalahan:', error);
+            }
+        });
     </script>
 </body>
 
