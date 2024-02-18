ee389907f63ac69014d6db2b4cad186fa5e63cd9
adyfp24 <adyfp24@gmail.com>
2024-02-18 20:30:48 +0700

    fix(login administrator): fixing login page for dashboard's admin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,21 +10,21 @@
 </head>
 
 <body>
-    <div class="container">
-        <h2 class="text-center">Login</h2>
+    <div class="container mt-5 pt-5 mx-auto" style="width: 40%;">
+        <h2 class="text-center mb-4">Login Administrator</h2>
         <form action="{{ route('login') }}" method="POST">
-        @csrf
-          <div class="form-group mb-4 ">
-            <label for="username">Username</label>
-            <input type="text" class="form-control" id="username" name="username" placeholder="Enter username">
-          </div>
-          <div class="form-group mb-4">
-            <label for="password">Password</label>
-            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-          </div>
-          <button type="submit" class="btn btn-primary btn-block">Login</button>
+            @csrf
+            <div class="mb-3">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" class="form-control" id="username" name="username" placeholder="Enter username">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+            </div>
+            <button type="submit" class="btn btn-primary w-100 btn-block">Login</button>
         </form>
-      </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
